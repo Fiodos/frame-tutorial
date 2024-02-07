fb662c9f0c1eea53f4bf9008774004d2f157675e
Arcangelo <fiodar15@googlemail.com>
2024-02-07 13:46:17 +0100

    changed id inside post_url

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const frameMetadata = getFrameMetadata({
     }
   ],
   image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${cid}/45.png`,
-  post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
+  post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=45`,
 });
 
 export const metadata: Metadata = {
