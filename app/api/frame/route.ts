ca32ffc81c6a9bcef1ede22586a6cb6c0c3946b6
Arcangelo <fiodar15@googlemail.com>
2024-02-07 16:17:14 +0100

    changes

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -13,12 +13,8 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     <title>This is frame 70</title>
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmU3VNMc91F9oDZcyAJuSnK5hB9FzM6mbfxu6t5qqeRAvW/70.png" />
-    <meta property="fc:frame:button:1" content="Blockstories" />
+    <meta property="fc:frame:button:1" content="Full Startup Profiles (German)" />
     <meta property="fc:frame:button:1:action" content="post_redirect" />
-    <meta property="fc:frame:button:2" content="Blog post Tutorial" />
-    <meta property="fc:frame:button:2:action" content="post_redirect" />
-    <meta property="fc:frame:button:3" content="Video Tutorial" />
-    <meta property="fc:frame:button:3:action" content="post_redirect" />
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
   </head></html>`);
   } else {
--- next.config.mjs
+++ next.config.mjs
@@ -5,17 +5,7 @@ const nextConfig = {
     return [
       {
         source: '/blockstories',
-        destination: 'https://blockstories.de',
-        permanent: true
-      },
-      {
-        source: '/pinatacloud',
-        destination: 'https://www.pinata.cloud/blog/how-to-make-a-frame-on-farcaster-using-ipfs',
-        permanent: true
-      },
-      {
-        source: '/video',
-        destination: 'https://youtu.be/wUt5NjXHSO4',
+        destination: 'https://blockstories.de/startups-to-watch',
         permanent: true
       }
     ]
